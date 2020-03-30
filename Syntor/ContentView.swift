8b5840e6e238eafe0f16cdc1e17f5c32e868ce7c
Evgenii Pliusnin <eninbox@gmail.com>
2020-03-30 21:01:14 +0500

    Initial Commit

---
+++ Syntor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Syntor
+//
+//  Created by Евгений Плюснин on 30.03.2020.
+//  Copyright © 2020 Evgenii Pliusnin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
